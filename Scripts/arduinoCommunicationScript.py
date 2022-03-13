185694a064f0f2c0e8795463de0d34a6042511f5
Jeffrey Aaron Jeyasingh <jeffreyaaron06@gmail.com>
2022-03-12 23:02:07 -0800

    Update arduino communication

--- Scripts/arduinoCommunicationScript.py
+++ Scripts/arduinoCommunicationScript.py
@@ -4,22 +4,47 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout = 0)
 
 targetAngle = 180
 
-while True:
-	try :
-		# read angle
-		arduino.write('M'.encode())
-		time.sleep(0.01) # some time for measurement to be taken
-		incomingData = str(arduino.readline().strip(), 'utf-8')
-		# print(incomingData)
-		print(float(incomingData))
+'''
+Arduino Code Reference:
 
-		currentAngle = float(incomingData)
+Retrieval
+M - get current compass (server motor) angle relative to north
+
+Commands
+L - set servo motor to turn left
+R - set servo motor to turn right
+'''
+
+def scriptLoop():
+	while True:
+		handleServoMotor()
+
+def handleServoMotor():
+		# read angle
+		currentAngle = retrieveValueFromArduino('M')
+
+		print(currentAngle)
 
 		if(currentAngle > targetAngle):
-			arduino.write('R'.encode())
+			sendCommandToArduino('R')
 		else:
-			arduino.write('L'.encode())
-	except ValueError as e:
-		pass
-		# invalid data
-		# print( e)
+			sendCommandToArduino('L')
+
+def retrieveValueFromArduino(code, number=True):
+	output = 0
+	arduino.write(code.encode())
+	time.sleep(0.01) # some time for measurement to be taken
+	if(number):
+		incomingData = str(arduino.readline().strip(), 'utf-8')
+		try :
+			output = float(incomingData)
+		except ValueError:
+			pass
+
+	return output
+
+def sendCommandToArduino(code):
+	arduino.write(code.encode())
+
+if __name__ == "__main__":
+	scriptLoop()
